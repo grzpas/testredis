6de8234c8702f6e01408b5fb346cafb58fb2d4cc
SG0893950 <grzegorz.pasternak@sabre.com>
2020-03-14 20:48:55 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Poland");
+        }
+    }
+}
